bc8b4faccf180b08d9a34e74f3b923ad21773abc
Shubham Vasaikar <shubham.vasaikar7@gmail.com>
2018-11-12 11:05:00 -0800

    Write temperature values to Firebase and set DC Motor PWM Duty cycle.

--- AutoHomeHardware/app/src/main/java/com/vasaikar/autohomehardware/MainActivity.java
+++ AutoHomeHardware/app/src/main/java/com/vasaikar/autohomehardware/MainActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.bluetooth.BluetoothClass;
 import android.content.Intent;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.support.annotation.NonNull;
 import android.util.Log;
 
@@ -38,7 +39,7 @@ import java.util.Locale;
  * @see <a href="https://github.com/androidthings/contrib-drivers#readme">https://github.com/androidthings/contrib-drivers#readme</a>
  */
 public class MainActivity extends Activity {
-    public final String TAG = "AutoHomeHardware";
+    public final String TAG = "com.vasaikar.AutoHomeHardware";
 
     I2cDevice mDevice;
 
@@ -46,6 +47,8 @@ public class MainActivity extends Activity {
     DatabaseReference mPwm3DbRef;
     DatabaseReference mPwm4DbRef;
     DatabaseReference mPwm5DbRef;
+    DatabaseReference mPwm6DbRef;
+    DatabaseReference mTempDbRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -64,6 +67,8 @@ public class MainActivity extends Activity {
         mPwm3DbRef = mDbRef.child("PWM3");
         mPwm4DbRef = mDbRef.child("PWM4");
         mPwm5DbRef = mDbRef.child("PWM5");
+        mPwm6DbRef = mDbRef.child("PWM6");
+        mTempDbRef = mDbRef.child("ADA5IN");
 
         mPwm3DbRef.addValueEventListener(new ValueEventListener() {
             @Override
@@ -115,6 +120,52 @@ public class MainActivity extends Activity {
                 Log.d(TAG, "Cancelled");
             }
         });
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    while (true) {
+                        int lsb = (int) mDevice.readRegByte(0x05);
+                        int msb = (int) mDevice.readRegByte(0x06);
+                        // Log.d(TAG, "LSB: " + lsb + " MSB: " + msb);
+                        double temp = ((msb & 0xFF << 8) | lsb & 0xFF) / 10.0;
+                        mTempDbRef.setValue(temp);
+                        setMotorSpeed(temp);
+                        // Log.d(TAG, "Temp: " + temp);
+                        Thread.sleep(1000);
+                    }
+                } catch (IOException | InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+            private void setMotorSpeed(double temp) {
+                try {
+                    if (temp <= 15) {
+                        mDevice.writeRegByte(0x03, (byte) 80);
+                        mPwm6DbRef.setValue(80);
+                    }
+                    else if (temp > 15 && temp <= 18) {
+                        mDevice.writeRegByte(0x03, (byte) 30);
+                        mPwm6DbRef.setValue(30);
+                    }
+                    else if (temp > 18 && temp <= 22) {
+                        mDevice.writeRegByte(0x03, (byte) 50);
+                        mPwm6DbRef.setValue(50);
+                    }
+                    else if (temp > 22 && temp <= 25) {
+                        mDevice.writeRegByte(0x03, (byte) 70);
+                        mPwm6DbRef.setValue(70);
+                    }
+                    else {
+                        mDevice.writeRegByte(0x03, (byte) 40);
+                        mPwm6DbRef.setValue(40);
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
     }
 
     @Override
@@ -128,5 +179,9 @@ public class MainActivity extends Activity {
                 Log.w(TAG, "Unable to close I2C device", e);
             }
         }
+    }
+
+    public void readTemperature() {
+
     }
 }
